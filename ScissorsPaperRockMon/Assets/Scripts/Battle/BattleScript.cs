d3da79c02a55242638beaa28b6d8b1e59e1d9fb4
Sholto Tiger Collins <tiger.collins@outlook.com>
2016-11-27 08:53:32 +1000

    Buttons in battle scene now change the players attack move. Fixed UI scaling on battle scene.

---
+++ ScissorsPaperRockMon/Assets/Scripts/Battle/BattleScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleScript : MonoBehaviour {
+    public GameObject[] PlayerBattleOptions;
+    public int[] BattleOptions;
+    int index;
+
+    void Start()
+    {
+        index = Random.Range(0, BattleOptions.Length);
+        print(index);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void BattleScissors()
+    {
+        if (PlayerBattleOptions[0].activeInHierarchy == false) //Checks to see if the object is active. If not active...
+        {
+            PlayerBattleOptions[0].SetActive(true); //... Set as active.
+            PlayerBattleOptions[1].SetActive(false); // Disable this in hierarchy
+            PlayerBattleOptions[2].SetActive(false); // Disable this in hierarchy
+        }
+    }
+
+    public void BattlePaper()
+    {
+        if (PlayerBattleOptions[1].activeInHierarchy == false) //Checks to see if the object is active. If not active...
+        {
+            PlayerBattleOptions[1].SetActive(true); //... Set as active.
+            PlayerBattleOptions[0].SetActive(false); // Disable this in hierarchy
+            PlayerBattleOptions[2].SetActive(false); // Disable this in hierarchy
+        }
+    }
+
+    public void BattleRock()
+    {
+        if (PlayerBattleOptions[2].activeInHierarchy == false) //Checks to see if the object is active. If not active...
+        {
+            PlayerBattleOptions[2].SetActive(true); //... Set as active.
+            PlayerBattleOptions[0].SetActive(false); // Disable this in hierarchy
+            PlayerBattleOptions[1].SetActive(false); // Disable this in hierarchy
+        }
+    }
+}
+
